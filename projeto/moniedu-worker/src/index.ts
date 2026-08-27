ee8b4ac0b62855285835e85d437bdacb459827da
Rubens <grubens476@gmail.com>
2026-08-27 05:51:00 -0300

    erro de cors

--- projeto/moniedu-worker/src/index.ts
+++ projeto/moniedu-worker/src/index.ts
@@ -1,9 +1,24 @@
 
 import { Hono } from "hono";
+import { cors } from "hono/cors";
+
 import { auth } from "./auth";
 
 
 const app = new Hono();
+
+app.use(
+	"/api/*",
+	cors({
+		origin: [
+			"http://localhost:3000",
+			"https://monitoraedu.vercel.app"
+		],
+		allowMethods: ["GET", "POST", "OPTIONS"],
+		allowHeaders: ["Content-Type", "Authorization"],
+		credentials: true,
+	})
+);
 
 app.get("/health", (c) => c.json({ 'status': 'Funcionando!' }));
 
