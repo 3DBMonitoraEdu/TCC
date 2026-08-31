d09100f6a448cc67fc8e92300e6396ef058ac7ec
Rubens <grubens476@gmail.com>
2026-08-30 21:46:53 -0300

    removing SignUp

--- projeto/moniedu-worker/src/auth.ts
+++ projeto/moniedu-worker/src/auth.ts
@@ -11,7 +11,7 @@ export const auth = betterAuth({
 	trustedOrigins: TRUSTED_ORIGINS,
 	emailAndPassword: {
 		enabled: true,
-		//disableSignUp: true,
+		disableSignUp: true,
 	},
 	rateLimit: {
 		enabled: true,
